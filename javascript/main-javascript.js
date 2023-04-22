04c16305be35cb3126d41a63e77b2a16e2425e8b
jonah-day <jonahday25@gmail.com>
2023-04-22 03:19:37 -0700

    Reformatted styles and rough breakpoints for tablet

--- javascript/main-javascript.js
+++ javascript/main-javascript.js
@@ -1,15 +1,20 @@
 
 
 let menuButton = document.querySelector(".main-dropdown");
+let logoLayer = document.querySelector(".logo-container");
 let isClicked = false;
 
 function clickDropMenu() {
     if (isClicked) {
       document.documentElement.style.setProperty("--dropdown-box-width", "54px");
+      logoLayer.style.visibility = "visible";
+      logoLayer.style.opacity = "1";
       isClicked = false;
     }
     else {
       document.documentElement.style.setProperty("--dropdown-box-width", "100%");
+      logoLayer.style.opacity = "0";
+      logoLayer.style.visibility = "hidden";
       isClicked = true;
     }
 }
